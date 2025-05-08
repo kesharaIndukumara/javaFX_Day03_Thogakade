368b8750e8727b948a89a68a63661b95c83ca380
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-05-08 11:49:02 +0530

    Coomit

---
+++ src/main/java/controller/CustomerFormController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CustomerFormController {
+
+    @FXML
+    private Button BtnDelete;
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnReload;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSearchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+}
