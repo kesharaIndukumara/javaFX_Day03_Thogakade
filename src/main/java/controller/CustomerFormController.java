6c1a638c6835501100bf8eae08d070d6f36aced3
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-05-09 14:50:47 +0530

    add customer data to table

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -1,16 +1,31 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.Customer;
 
+import java.awt.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerFormController {
+
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colSalary;
+    public TableView tblCustomerView;
 
     @FXML
     private Button BtnDelete;
@@ -69,13 +84,31 @@ public class CustomerFormController {
             Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/thoga_kade","root","1234");
             ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM customer");
 
-            while(resultSet.next())
-                System.out.println(resultSet.getString(2));
+            List<Customer> customerList = new ArrayList<>();
+
+            while(resultSet.next()) {
+                customerList.add(new Customer(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getDouble(4)));
+            }
+
+            colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+            colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+            colAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+            colSalary.setCellValueFactory(new PropertyValueFactory<>("salary"));
+
+            ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
+
+            customerList.forEach(customer -> {
+                customerObservableList.add(customer);
+            });
+
+            tblCustomerView.setItems(customerObservableList);
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
+
 
 
 }
---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,16 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Customer {
+    private Integer id;
+    private String name;
+    private String address;
+    private Double salary;
+}
