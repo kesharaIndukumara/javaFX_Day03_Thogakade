8b1df890359ac6b68e194a23bff4d64a9a1e8432
kesharaIndukumara <kavindukeahara2016@gmail.com>
2025-05-09 08:41:13 +0530

    get customer names

--- src/main/java/controller/CustomerFormController.java
+++ src/main/java/controller/CustomerFormController.java
@@ -5,6 +5,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class CustomerFormController {
 
     @FXML
@@ -46,7 +51,7 @@ public class CustomerFormController {
 
     @FXML
     void btnReloadOnAction(ActionEvent event) {
-
+        loadTable();
     }
 
     @FXML
@@ -58,5 +63,19 @@ public class CustomerFormController {
     void btnUpdateOnAction(ActionEvent event) {
 
     }
+
+    private void loadTable(){
+        try {
+            Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/thoga_kade","root","1234");
+            ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM customer");
+
+            while(resultSet.next())
+                System.out.println(resultSet.getString(2));
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 }
